a445c819dbb97edf5bde268ca1784f61aaeccc0e
Morgan Eggleston <37636408+morgan-eggleston@users.noreply.github.com>
2019-01-01 20:12:21 -0700

    Update settings.py

--- DjangoDTD/settings.py
+++ DjangoDTD/settings.py
@@ -148,6 +148,6 @@ MESSAGE_TAGS = {
 #email config
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'morganeggleston59@gmail.com'
-EMAIL_HOST_PASSWORD = 'meggles1'
+EMAIL_HOST_USER = 
+EMAIL_HOST_PASSWORD = 
 EMAIL_USE_TLS = True
